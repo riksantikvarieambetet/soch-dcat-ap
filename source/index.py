e3c7b49c6eec320cae80e2a341b86eb0f1aa2f0c
abbe98 <albin.post@gmail.com>
2018-02-01 16:56:04 +0100

    first commit

---
+++ source/index.py
@@ -0,0 +1,67 @@
+import os
+import requests
+from lxml import etree
+
+def insert_values(template, key, value):
+  template.replace(key, value)
+
+#TODO remove/generate API key and move to settings file
+service_index_source = 'http://www.kulturarvsdata.se/ksamsok/api?method=getServiceOrganization&value=all&x-api=test'
+
+r = requests.get(service_index_source)
+xml = etree.XML(r.content)
+
+institutions = list()
+services = list()
+
+for institution_node in xml.xpath('/result/institution'):
+    institution = {}
+    institution['name_sv'] = institution_node.xpath('.//namnswe')[0].text or ''
+    institution['name_en'] = institution_node.xpath('.//namneng')[0].text or ''
+    institution['webpage'] = institution_node.xpath('.//websida')[0].text or ''
+    institution['contact_email'] = institution_node.xpath('.//epostkontaktperson')[0].text or ''
+
+    institutions.append(institution)
+
+    for service_node in institution_node.xpath('.//services/service'):
+        service = {}
+        service['title'] = service_node.xpath('.//namn')[0].text or ''
+        service['description'] = service_node.xpath('.//beskrvning')[0].text or ''
+        service['contact_email'] = institution['contact_email']
+        service['publisher_url'] = institution['webpage']
+
+        services.append(service)
+        del service
+
+    del institution
+
+agent_template_file = open('source/agent-template.xml', 'r')
+agent_template = agent_template_file.read()
+agent_template_file.close()
+
+dataset_template_file = open('source/dataset-template.xml', 'r')
+dataset_template = dataset_template_file.read()
+dataset_template_file.close()
+
+root_template_file = open('source/root-template.rdf', 'r')
+root_template = root_template_file.read()
+root_template_file.close()
+
+agents_output = ''
+datasets_output = ''
+
+for agent in institutions:
+    agents_output += agent_template.replace('{ webpage }', agent['webpage']).replace('{ email }', agent['contact_email']).replace('{ name_en }', agent['name_en']).replace('{ name_sv }', agent['name_sv'])
+
+
+
+for dataset in services:
+    datasets_output += dataset_template.replace('{ description }', dataset['description']).replace('{ publisher }', dataset['publisher_url']).replace('{ title }', dataset['title']).replace('{ email }', dataset['contact_email'])
+
+final_data = root_template.replace('{ datasets }', datasets_output).replace('{ agents }', agents_output)
+
+os.makedirs(os.path.dirname('output/soch.rdf'), exist_ok=True)
+with open('output/soch.rdf', 'w', encoding='utf-8') as f:
+    f.write(final_data)
+
+print('DONE')
